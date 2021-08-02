0cd63a4a906779e581be47e7733ba194c5ea8881
nikhil <nikhil@asus>
2021-08-02 23:39:00 +0530

    IP Address changed

--- signatureAddition/src/main/java/signatureAddition/DataBaseConnect.java
+++ signatureAddition/src/main/java/signatureAddition/DataBaseConnect.java
@@ -10,7 +10,7 @@ public class DataBaseConnect {
 		// TODO Auto-generated method stub
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			String sqlurl="jdbc:mysql://10.192.38.90:3306/apps";
+			String sqlurl="jdbc:mysql://localhost:3306/apps";
 			Connection cn=DriverManager.getConnection(sqlurl,"nikhil","nikhil");
 			Statement st=cn.createStatement();
 			return st;
