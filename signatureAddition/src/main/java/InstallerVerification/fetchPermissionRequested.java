f390d6757f44da83a18655ed29193b538bb39aca
nikhil <nikhil@asus>
2021-08-08 12:52:57 +0530

    grant all permission through aapt

---
+++ signatureAddition/src/main/java/InstallerVerification/fetchPermissionRequested.java
@@ -0,0 +1,30 @@
+package InstallerVerification;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import analysingDumpSys.DumpSysAnalysis;
+import signatureAddition.CommandExecute;
+import signatureAddition.LogAnalysis;
+
+public class fetchPermissionRequested {
+	public static void grantPermissions(String packageName, String pathToApk ) throws Exception{
+		Process process=CommandExecute.commandExecution(DumpSysAnalysis.pathToaapt+" dump badging "+pathToApk);
+		BufferedReader bufferedReader= new BufferedReader(new InputStreamReader(process.getInputStream()));
+		String line=bufferedReader.readLine();
+		String pattern="uses-permission: name='";
+		//String packageName="in.org.npci.upiapp";
+		while(line!=null)
+		{
+			System.out.println(line);
+			if(line.contains(pattern))
+			{
+				String permission=line.substring(pattern.length(),line.length()-1);
+				System.out.println(permission);
+				CommandExecute.commandExecution(LogAnalysis.pathToadb+" shell pm grant "+packageName+" "+permission);
+			}
+			line=bufferedReader.readLine();
+		}
+
+	}
+}
